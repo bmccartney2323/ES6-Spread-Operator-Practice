270d5990b391d97dc636d7f8d137ff206a84a428
bmccartney2323 <65781099+bmccartney2323@users.noreply.github.com>
2020-11-14 15:24:12 -0500

    Completed JS ES6 Spread Operator Practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React,  { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+// Initial state for items is an empty array
+const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  };
+  function addItem(event){
+    // here we display all the items.
+    setItems( (prevItems) => {
+      return [...prevItems, inputText];
+    });
+    // Here we clear the input text from the display
+    setInputText("");
+  }
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
+        {/* Added a value property to input */}
+        <input onChange={handleChange} type="text" value= {inputText}/>
         <button>
-          <span>Add</span>
+          <span onClick={addItem}>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map(todoItem => { 
+            return <li>{todoItem}</li>
+          })}
+          {/* <li>A Item </li> */}
         </ul>
       </div>
     </div>
